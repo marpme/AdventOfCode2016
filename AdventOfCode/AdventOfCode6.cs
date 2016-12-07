33581be1ea13c11cb952fb56d93b98ee73f5d232
Kyonleague <cryzar95@googlemail.com>
2016-12-07 07:06:56 +0100

    Welcome
    added solutions for puzzle 5 and 6
    Number 1 is coming soon, missed the start :(

---
+++ AdventOfCode/AdventOfCode6.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class AdventOfCode6
+    {
+
+        private static Dictionary<char, int>[] _a;
+
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(
+                @"C:\Users\kyon\Documents\GitGood\AdventOfCode2016\AdventOfCode\items.txt");
+
+            _a = new Dictionary<char, int>[lines[0].Length];
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                _a[i] = new Dictionary<char, int>();
+            }
+
+            foreach (string line in lines)
+            {
+                ProcessItems(line);
+            }
+
+            foreach (Dictionary<char, int> dictionary in _a)
+            {
+                var myList = dictionary.ToList();
+                // Lowest first +1 | highest first -1
+                myList.Sort((pair1, pair2) => -1 * pair1.Value.CompareTo(pair2.Value));
+                Console.Write(myList[0].Key);
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void ProcessItems(object obj)
+        {
+            var str = obj as string;
+            if (str == null)
+            {
+                throw new Exception();
+            }
+
+            for (var c = 0; c < str.Length; c++)
+            {
+                if (_a[c].ContainsKey(str[c]))
+                {
+                    _a[c][str[c]]++;
+                }
+                else
+                {
+                    _a[c].Add(str[c], 1);
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode_5/AdventOfCode5.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode_5
+{
+    class AdventOfCode5
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            string hash;
+            var doorId = "cxdnnyjw";
+            var key = new char[8];
+            var fin = 0;
+
+            while (fin != 8)
+            {
+                hash = "Kappapa";
+                while (hash.Substring(0, 5) != "00000")
+                {
+                    hash = CalculateMd5Hash(doorId + count);
+                    count++;
+                }
+
+                try
+                {
+                    int a = (int)char.GetNumericValue(hash[5]);
+                    if (a <= 7 && key[a] == '\0')
+                    {
+                        fin++;
+                        key[a] += hash[6];
+                        Console.WriteLine("found key " + fin + " for pos " + a + " at " + count);
+                    }
+                    else
+                    {
+                        Console.WriteLine("key ~ at " + count);
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("key ~ at " + count);
+                }
+
+            }
+            
+            Console.WriteLine(new string(key) + " | " + count);
+            Console.ReadLine();
+        }
+
+        private static string CalculateMd5Hash(string input)
+        {
+            var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hash = md5.ComputeHash(inputBytes);
+            return ByteArrayToString(hash);
+        }
+
+        private static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+    }
+}
